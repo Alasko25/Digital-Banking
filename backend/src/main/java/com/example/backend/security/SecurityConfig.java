ef8aec4a5fd6aa45b95d7c0e520227aa74e37544
Alasko <alassane.toure-etu@etu.univh2c.ma>
2025-06-09 17:11:50 +0100

    last of the day

--- backend/src/main/java/com/example/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.example.backend.security;
 
 import com.nimbusds.jose.jwk.source.ImmutableSecret;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -35,65 +34,65 @@ import javax.crypto.spec.SecretKeySpec;
 @EnableMethodSecurity()
 
 public class SecurityConfig {
-  /*//@Value("${jwt.secret}")
-  private String secretKey;
-  @Bean
-  public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
-    PasswordEncoder passwordEncoder = passwordEncoder();
-    return new InMemoryUserDetailsManager(
-      User.withUsername("user1").password(passwordEncoder.encode("1234")).authorities("USER").build(),
-      User.withUsername("admin").password(passwordEncoder.encode("1234")).authorities("USER","ADMIN").build()
-    );
+    //@Value("${jwt.secret}")
+    private String secretKey;
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        PasswordEncoder passwordEncoder = passwordEncoder();
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password(passwordEncoder.encode("1234")).authorities("USER").build(),
+                User.withUsername("admin").password(passwordEncoder.encode("1234")).authorities("USER","ADMIN").build()
+        );
 
-  }
-  @Bean
-  public PasswordEncoder passwordEncoder(){
-    return new BCryptPasswordEncoder();
-  }
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-    return  httpSecurity
-      .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-      .csrf(AbstractHttpConfigurer::disable)
-      .cors(Customizer.withDefaults())
-      .authorizeHttpRequests(ar -> ar.requestMatchers("/auth/login/**").permitAll())
-      .authorizeHttpRequests(ar -> ar.anyRequest().authenticated())
-      //.httpBasic(Customizer.withDefaults())
-      .oauth2ResourceServer(oa -> oa.jwt(Customizer.withDefaults()))
-      .build();
-  }
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return  httpSecurity
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(ar -> ar.requestMatchers("/auth/login/**").permitAll())
+                .authorizeHttpRequests(ar -> ar.anyRequest().authenticated())
+                //.httpBasic(Customizer.withDefaults())
+                .oauth2ResourceServer(oa -> oa.jwt(Customizer.withDefaults()))
+                .build();
+    }
 
-  @Bean
-  JwtEncoder jwtEncoder() {
-    String secretKey="9faa372517ac1d389758d3750fc07acf00f542277f26fec1ce4593e93f64e338";
-    return new NimbusJwtEncoder(new ImmutableSecret<>(secretKey.getBytes()));
-  }
+    @Bean
+    JwtEncoder jwtEncoder() {
+        String secretKey="9faa372517ac1d389758d3750fc07acf00f542277f26fec1ce4593e93f64e338";
+        return new NimbusJwtEncoder(new ImmutableSecret<>(secretKey.getBytes()));
+    }
 
-  @Bean
-  JwtDecoder jwtDecoder() {
-    String secretKey="9faa372517ac1d389758d3750fc07acf00f542277f26fec1ce4593e93f64e338";
-    SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "RSA");
-    return NimbusJwtDecoder.withSecretKey(secretKeySpec).macAlgorithm(MacAlgorithm.HS512).build();
-  }
+    @Bean
+    JwtDecoder jwtDecoder() {
+        String secretKey="9faa372517ac1d389758d3750fc07acf00f542277f26fec1ce4593e93f64e338";
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "RSA");
+        return NimbusJwtDecoder.withSecretKey(secretKeySpec).macAlgorithm(MacAlgorithm.HS512).build();
+    }
 
-  @Bean
-  public AuthenticationManager authenticationManager(UserDetailsService userDetailsService){
-    DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-    daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-    daoAuthenticationProvider.setUserDetailsService(userDetailsService);
-    return new ProviderManager(daoAuthenticationProvider);
-  }
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        return new ProviderManager(daoAuthenticationProvider);
+    }
 
 
-  @Bean
-  CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration corsConfiguration = new CorsConfiguration();
-    corsConfiguration.addAllowedOrigin("*");
-    corsConfiguration.addAllowedMethod("*");
-    corsConfiguration.addAllowedHeader("*");
-    //corsConfiguration.setExposedHeaders(List.of("x-auth-token"));
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", corsConfiguration);
-    return source;
-  }*/
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+        //corsConfiguration.setExposedHeaders(List.of("x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
 }
--- backend/src/main/java/com/example/backend/security/SecurityController.java
+++ backend/src/main/java/com/example/backend/security/SecurityController.java
@@ -24,38 +24,38 @@ import java.util.stream.Collectors;
 @RequestMapping("/auth")
 @AllArgsConstructor
 public class SecurityController {
-  /*private AuthenticationManager authenticationManager;
-  private JwtEncoder jwtEncoder;
-  @GetMapping("/profile")
-  public Authentication authentication(Authentication authentication){
-    return authentication;
-  }
+    private AuthenticationManager authenticationManager;
+    private JwtEncoder jwtEncoder;
+    @GetMapping("/profile")
+    public Authentication authentication(Authentication authentication){
+        return authentication;
+    }
 
-  @PostMapping("/login")
-  public Map<String, String> login(String username, String password){
-    Authentication authentication = authenticationManager.authenticate(
-      new UsernamePasswordAuthenticationToken(username, password)
-    );
-    Instant instant = Instant.now();
-    String scope = authentication.getAuthorities().stream()
-      .map(GrantedAuthority::getAuthority)
-      .collect(Collectors.joining(" "));
+    @PostMapping("/login")
+    public Map<String, String> login(String username, String password){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(username, password)
+        );
+        Instant instant = Instant.now();
+        String scope = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
 
-    JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder()
-      .issuedAt(instant)
-      .expiresAt(instant.plus(10, ChronoUnit.MINUTES))
-      .subject(username)
-      .claim("scope", scope)
-      .build();
+        JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder()
+                .issuedAt(instant)
+                .expiresAt(instant.plus(10, ChronoUnit.MINUTES))
+                .subject(username)
+                .claim("scope", scope)
+                .build();
 
-    JwtEncoderParameters jwtEncoderParameters = JwtEncoderParameters.from(
-      JwsHeader.with(MacAlgorithm.HS512).build(),
-      jwtClaimsSet
-    );
+        JwtEncoderParameters jwtEncoderParameters = JwtEncoderParameters.from(
+                JwsHeader.with(MacAlgorithm.HS512).build(),
+                jwtClaimsSet
+        );
 
-    String jwt = jwtEncoder.encode(jwtEncoderParameters).getTokenValue();
-    return Map.of("access-token", jwt);
-  }
+        String jwt = jwtEncoder.encode(jwtEncoderParameters).getTokenValue();
+        return Map.of("access-token", jwt);
+    }
 
-*/
+
 }
